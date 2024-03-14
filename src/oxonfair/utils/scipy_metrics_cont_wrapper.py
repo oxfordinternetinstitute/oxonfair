7fed88f41442f4459069f5bca3b4762ed839350f
Chri Russell <christopher.m.russell@gmail.com>
2024-03-14 11:29:39 +0000

    enable verbose and return original by default

---
+++ src/oxonfair/utils/scipy_metrics_cont_wrapper.py
@@ -0,0 +1,16 @@
+# Wrapper for scipy continuious metrics
+
+from typing import Any
+from sklearn.metrics import roc_auc_score, average_precision_score
+
+class ScorerRequiresContPred:
+    def __init__(self, scorer, name) -> None:
+        self.scorer = scorer
+        self.name = name
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.scorer(*args, **kwds)
+    
+
+roc_auc = ScorerRequiresContPred(roc_auc_score,'ROC AUC')
+average_precision = ScorerRequiresContPred(average_precision_score, 'Average Precision')
