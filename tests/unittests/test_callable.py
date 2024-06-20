b90830e1566edfbbd15a26a5417b527d19dc6c9c
Chris Russell <chris.russell@oii.ox.ac.uk>
2024-06-20 17:27:17 +0100

    improve test stability

--- tests/unittests/test_callable.py
+++ tests/unittests/test_callable.py
@@ -27,9 +27,9 @@ test_dict = {"data": test, "target": test_target, "groups": test_groups}
 def test_runs(use_fast=True):
     fpred = oxonfair.FairPredictor(sigmoid, val_dict, val_groups, inferred_groups=square_align,
                                    use_fast=use_fast)
-    fpred.fit(gm.accuracy, gm.equal_opportunity, 0.002)
+    fpred.fit(gm.accuracy, gm.equal_opportunity, 0.005)
     tmp = np.asarray(fpred.evaluate(metrics={'eo': gm.equal_opportunity}))[0, 1]
-    assert tmp < 0.002
+    assert tmp < 0.005
     fpred.plot_frontier()
     fpred.plot_frontier(test_dict)
     fpred.evaluate()
@@ -50,9 +50,9 @@ def test_runs_hybrid():
 
 def test_fairdeep(use_fast=True, use_true_groups=False):
     fpred = oxonfair.DeepFairPredictor(val_target, val, val_groups, use_fast=use_fast, use_actual_groups=use_true_groups)
-    fpred.fit(gm.accuracy, gm.equal_opportunity, 0.002)
+    fpred.fit(gm.accuracy, gm.equal_opportunity, 0.005)
     tmp = np.asarray(fpred.evaluate(metrics={'eo': gm.equal_opportunity}))[0, 1]
-    assert tmp < 0.002
+    assert tmp < 0.005
     fpred.plot_frontier()
     fpred.plot_frontier(test_dict)
     fpred.evaluate()
