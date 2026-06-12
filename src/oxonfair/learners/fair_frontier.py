52f1eea06a11d600c79d9fdc132cb6a2843c86d7
Chris Russell <christopher.m.russell@gmail.com>
2026-06-12 17:18:20 +0100

    Comment clean-up

--- src/oxonfair/learners/fair_frontier.py
+++ src/oxonfair/learners/fair_frontier.py
@@ -82,8 +82,6 @@ def sort_by_front(front: np.ndarray, weights: np.ndarray) -> Tuple[np.ndarray, n
     front = front[:, sort_ind]
     return front, weights
 
-# Solution modified from here:
-# https://stackoverflow.com/questions/32791911/fast-calculation-of-pareto-front-in-python
 
 
 def linear_interpolate(front: np.ndarray, weights: np.ndarray, gap: float = 0.01) -> np.ndarray:
@@ -243,6 +241,8 @@ def build_coarse_to_fine_front(metrics: Sequence[Callable],
                                        logit_scaling=logit_scaling)
     front = front_from_weights(weights, y_true, proba, groups_infered, metrics)
     front, weights = keep_front(front, weights, directions, additional_constraints, force_levelling_up=force_levelling_up)
+
+
     # second stage
     mins = weights[:, :-1].min(-1)  # drop zeros
     maxs = weights[:, :-1].max(-1)
@@ -261,6 +261,8 @@ def build_coarse_to_fine_front(metrics: Sequence[Callable],
         front = np.concatenate((existing_front, front), -1)
 
     front, weights = keep_front(front, weights, directions, additional_constraints, force_levelling_up=force_levelling_up)
+
+
     for _ in range(nr_of_recursive_calls - 1):
         if weights.shape[-1] != 1:
             eps /= refinement_factor
@@ -271,10 +273,12 @@ def build_coarse_to_fine_front(metrics: Sequence[Callable],
             front = np.concatenate((new_front, front), -1)
             front, weights = keep_front(front, weights, directions, additional_constraints, force_levelling_up=force_levelling_up)
 
+
     # densify the front with uniform interpolation
     if weights.shape[-1] > 1:
         weights = linear_interpolate(front, weights, gap=0.02)
         front = front_from_weights(weights, y_true, proba, groups_infered, metrics)
         front, weights = keep_front(front, weights, directions, additional_constraints, force_levelling_up=force_levelling_up)
+
 
     return front, weights
